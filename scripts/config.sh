a8579acb8be1c347629fd6e53b1bed40c5f69db4
JavierAbascal <javier.abascal@hotmail.com>
2015-07-18 19:04:20 +0200

    new update to send-capturedata.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,14 +3,14 @@
 # Working Variables
 export RasbperryPi2_ID="000001"
 
-export restWebservice=http://192.168.0.103:8080/RESTWebservice/rest/file/upload
+export restWebService=http://52.18.1.37:8080/RouterREST/rest/file/upload
 export webservice_token=c512623ef8144b3862f19739ccc9fd03
 
 export sendDataInterval=10  # In Seconds
 export captureDebugMode=0
 
 # Path & Routes variables
-export _baseDir=/tmp/scanning/
+export _baseDir=/home/magictaly/work/scanning/
 export _dumpFileName=dump_
 export _logFile=${_baseDir}log
 export _captureFile=$_baseDir$_dumpFileName
--- scripts/send-captured-data.sh
+++ scripts/send-captured-data.sh
@@ -10,7 +10,7 @@ extension=".log"
 prepend="probe_"
 separator="_"
 
-backedCaptureFileName=$_uploadDir$prepend$fileDate$separator$router_ID$extension
+backedCaptureFileName=$_uploadDir$prepend$fileDate$separator$RasbperryPi2_ID$extension
 mkdir -p "$_uploadDir"
 
 
@@ -20,7 +20,7 @@ if [ $# -eq 1 ] ; then
   rm $1
 fi
 
-# Compress log and capture files and deletes uncompressed ones after
+# Compress capture files
 cd $_uploadDir
 for f in probe_*.log ; do
   if [ -f "$f" ] ; then
@@ -29,17 +29,12 @@ for f in probe_*.log ; do
   fi
 done
 
-# Upload compressed files to server
-for f in ${_uploadDir}*.tar.gz ; do
+# Upload compressed files to server and deletes uploaded files
+for f in ${_uploadDir}*.gz ; do
   if [ -f "$f" ] ; then
     echo `date +"%Y-%m-%d_%H-%M-%S"`": Moving $f to server..." >> $_logFile
-    
-    # Webservice Php
-    # curl -f -F "method=putFile" -F "token=c512623ef8144b3862f19739ccc9fd02" -F "file=@$f" http://nutshell.gennion.com/counters/MacSnifferAPIService.php && rm "$f"
-	# Webservice Java Desarrollo
-	#curl -F "file=@$f" http://52.16.113.170:8080/RouterREST/rest/file/uploadRT?token=c512623ef8144b3862f19739ccc9fd02 && rm "$f"
-	#Webservice Java "Produccion"
-    #curl -F "file=@$f" ${restWebService}?token=${webservice_token} && rm "$f"
+    #Webservice Java "Produccion"
+    curl -F "file=@$f" ${restWebService}?token=${webservice_token} && rm "$f"
   fi
 done
 
